a77e47285578eaacc6545871d5820eb064917b4a
playdog-io <cqdxcs3@163.com>
2022-02-02 01:46:26 +0800

    able to interact with local testnet now

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -4,8 +4,17 @@
     <view class="text-area">
       <text class="title">{{ title }}</text>
     </view>
+
+    <view class="text-area">
+      <input type="text" v-model="greeting" /> <br />
+      <button @click="modify()">modify</button>
+      <button @click="fetchGreeting()">fetch</button>
+    </view>
   </view>
 </template>
+
+<!-- 
+
 
 <script>
 export default {
@@ -14,8 +23,85 @@ export default {
       title: "Hello",
     };
   },
-  onLoad() {},
+  onLoad() {
+    console.log("index onload");
+  },
   methods: {},
+};
+</script>
+
+ -->
+
+<script>
+import { ethers, providers } from "ethers";
+import Greeter from "../../src/artifacts/contracts/Greeter.sol/Greeter.json";
+console.log("ether: " + ethers);
+console.log("Greeter: " + Greeter);
+
+const greeterAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+console.log(greeterAddress);
+
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+
+  data() {
+    return {
+      title: "titile2",
+      greeting: "not yet generated",
+    };
+  },
+  onload() {
+    console.log("index onload");
+  },
+  async mounted() {
+    let that = this;
+    console.log("that is :" + that);
+    console.log("provider is :" + providers);
+  },
+  methods: {
+    async requestAccount() {
+      await window.ethereum.request({ method: "eth_requestAccounts" });
+    },
+
+    async fetchGreeting() {
+      if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const contract = new ethers.Contract(
+          greeterAddress,
+          Greeter.abi,
+          provider
+        );
+        try {
+          const data = await contract.greet();
+          this.greeting = data;
+          console.log("data: ", data);
+        } catch (err) {
+          console.log("Error: ", err);
+        }
+      }
+    },
+
+    async modify() {
+      let that = this;
+      if (!that.greeting) return;
+      if (typeof window.ethereum !== "undefined") {
+        await that.requestAccount();
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(
+          greeterAddress,
+          Greeter.abi,
+          signer
+        );
+        const transaction = await contract.setGreeting(that.greeting);
+        await transaction.wait();
+        that.fetchGreeting();
+      }
+    },
+  },
 };
 </script>
 
